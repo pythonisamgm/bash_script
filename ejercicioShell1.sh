aa7b8cebfcbfce4eb1cd8f187aec051d1a9cf9b8
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-05-16 15:31:01 +0200

    update the exercises

--- ejercicioShell1.sh
+++ ejercicioShell1.sh
@@ -61,11 +61,88 @@ yearsOld=$((actualDate - bornDate))
 echo "actualmente tienes $yearsOld años"
 }
 
+
+calcularEdad2(){
 read -p "ingresa tu fecha de nacimiento (ejemplo: 14/05/1984): " bornDate
+#componentes de la fecha de nacimiento
+bDay=$(echo $bornDate | cut -d'/' -f1)
+bMonth=$(echo $bornDate | cut -d'/' -f2)
+bYear=$(echo $bornDate | cut -d'/' -f3)
+bornDateInSeconds=$(date -d "$bYear-$bMonth-$bDay" "+%s")
 
-actualDateInSeconds=$(date +%s)
-bornDateInSeconds=$(date -d "$bornDate" "+%d/%m/%Y" +%s)
-echo "$bornDateInSeconds"
-yearsInSeconds=$((actualDateInSeconds - bornDateInSeconds))
+#componentes de la fecha actual
+currentDateInSeconds=$(date "+%s")
 
-echo "actualDateInSeconds - bornDateInSeconds $yearsInSeconds"
+#calcular la diferencia entre las fechas 
+differenceInSeconds=$((currentDateInSeconds - bornDateInSeconds))
+
+#restar segundos en un año y segundos totales de vida
+secondsInAYear=$((365*24*60*60))
+yearsOld=$((differenceInSeconds / secondsInAYear))
+
+echo "tienes $yearsOld años"
+}
+
+#BUCLES: ejercicio 1.Hacer un script que adivine el PID del script, que nos informe en cada momento si es mayor o menor el número ingresado. Cuando acertemos el número el script termina informandonos los números de intentos
+averiguarPid(){
+    
+pid=$$
+echo $pid
+contador=0
+gameIsOn="true"
+while [ "$gameIsOn" = "true" ]; do
+    read -p "ingresa un número para averiguar el PID" guessedPid 
+        if [ "$pid" -lt "$guessedPid" ]; then
+            echo "tu numero es mayor que el PID"
+            let contador=$contador+1
+
+        elif [ "$pid" -gt "$guessedPid" ]; then
+            echo "tu numero es menor que el PID"
+            let contador=$contador+1
+        elif [ "$pid" = "$guessedPid" ]; then
+            echo "acertaste en $contador numero de intentos"
+            gameIsOn="false"
+        fi
+done
+}
+
+#BUCLES: ejercicio 2. Hacer un script que:
+#Mediante un input podamos agregar un archivo (nos de un mensaje de confirmación)
+
+
+eliminarArchivo(){
+read -p "inserta el nombre de tu archivo. Recuerda incluir la extensión: " fileName
+newFile=$(touch $fileName)
+echo "has creado correctamente el archivo $fileName"
+
+deleteFile="Y"
+while [ "$deleteFile" = "Y" ]; do
+#Luego de 3 segundos de espera nos muestre la lista de archivos actual
+sleep 3
+currentFileList=$(ls)
+echo "Estos son los archivos que hay actualmente en esta carpeta: $currentFileList"
+#Nos pregunte si deseamos borrar un archivo
+read -p "¿quieres borrar algún archivo? Escribe Y o N: " deleteFile
+
+#De ser afirmativo, mediante un input podamos escribir por un lado el nombre del archivo, y por el otro  el formato
+if [ $deleteFile = "Y" ]; then
+    read -p "inserta el nombre del archivo: " fileNameToDelete
+    read -p "inserta el formato del archivo: " fileFormatToDelete
+
+#nos informe si el archivo fue encontrado y que lo borre
+    
+    fileToDelete="${fileNameToDelete}${fileFormatToDelete}"
+    echo "se ha encontrado el archivo $fileToDelete"
+    #findFile=$(find . -name $fileToDelete)
+    rm "$fileToDelete"
+    sleep 3
+    echo "se ha eliminado correctamente el archivo $fileToDelete"
+#En el caso de que no queramos borrar ningun archivo, nos de un mensaje de “Ok, no borraremos ningún archivo”
+
+else
+    echo 'Ok, no borraremos ningún archivo'
+
+fi
+done
+}
+eliminarArchivo
